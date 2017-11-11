2b90417a7349d5f9d2bd29ad1d643d8570064ff9
Paulo Felipe Piva dos Santos <paulo.f.piva@gmail.com>
2017-11-10 23:02:32 -0200

    Separado em pastas e inserido o projeto do visual studio. Pra abrir o projeto, abrir o arquivo .sln no visual studio
    Pra abrir o projeto, abrir o arquivo .sln no visual studio

---
+++ Xamarin/DIMO/DIMO/MainActivity.cs
@@ -0,0 +1,27 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace DIMO
+{
+    [Activity(Label = "Teste", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            Button button1 = FindViewById<Button>(Resource.Id.btnListarTurmas);
+            button1.Click += (sender, e) => {
+                
+            };
+
+            Button button2 = FindViewById<Button>(Resource.Id.btnCadastrarTurma);
+            button2.Click += (sender, e) => {
+
+            };
+            
+        }
+    }
+}
+
